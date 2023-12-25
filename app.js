a7da203677c1fb7422e6a3a7ea2c613c7c757aca
deepak <deepak@kali.kali>
2023-12-26 01:18:47 +0530

    fimit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(require("./routes/auth"));
 app.use(require("./routes/post"));
 app.use(require("./routes/user"));
 
-require("dotenv").config();
+
 const path = require("path");
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
