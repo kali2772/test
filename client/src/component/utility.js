8f9ec66574be80ed62eaf9901359c42a1d516d0d
deepak <deepak@kali.kali>
2023-12-28 01:34:30 +0530

    reusablity

---
+++ client/src/component/utility.js
@@ -0,0 +1,177 @@
+// toastUtils.js
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+const useBasicFunc = () => {
+  const [data, setData] = useState([]);
+
+  const showToast = (msg, toastType) => {
+    toast[toastType](msg, {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+    });
+  };
+  const share = (postid) => {
+    try {
+      const shareurl = `http://localhost:5000/posts/${postid}`;
+      navigator.clipboard.writeText(shareurl);
+      showToast("Copied to clipboard", "success");
+    } catch (error) {
+      console.log(error);
+      showToast("Failed to copy to clipboard", "error");
+    }
+  };
+  const savePost = (id) => {
+   return fetch("/savepost", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        localStorage.setItem("user", JSON.stringify(result.savepost));
+        showToast("post is saved", "success");
+        return result.savepost
+      })
+      .catch((err) => {
+        console.log(err);
+        showToast("sorry, but something wents wrong", "error");
+      });
+  };
+  const deletepost = (postid, cii) => {
+    // console.log(postid, cii);
+   return fetch(`/deletepost/${postid}/${cii}`, {
+      method: "delete",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        cii,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+      showToast("Post is deleted", "success");
+      return result;
+      });
+  };
+  const likePost = (id) => {
+    return fetch("/like", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = result.likes;
+        setData(newData);
+        return newData;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const unlikePost = (id) => {
+    return fetch("/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        const newData = result.likes;
+        setData(newData);
+        return newData;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  /* const unlikePost = (id) => {
+    fetch("/unlike", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId: id,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        const newData = data.map((item) => {
+          if (item._id === result.likes._id) {
+            return result.likes;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }; */
+  const makeComments = (text, postId) => {
+    fetch("/comment", {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        postId,
+        text,
+      }),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        const newData = data.map((item) => {
+          if (item._id === result.comments._id) {
+            return result.comments;
+          } else {
+            return item;
+          }
+        });
+        setData(newData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return {
+    showToast,
+    share,
+    savePost,
+    likePost,
+    unlikePost,
+    deletepost,
+    makeComments,
+  };
+};
+
+export default useBasicFunc;
